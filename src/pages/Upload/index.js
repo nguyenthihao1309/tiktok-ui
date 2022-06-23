fe2252d35248b19f8afcd20eda9b29a5ab6ce530
nguyenthihao1309 <thihaonguyen1309@gmail.com>
2022-06-23 15:38:58 +0700

    config layout

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,8 @@
+
+function Upload() {
+    return (
+        <div></div>
+    );
+}
+
+export default Upload;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,19 @@
+//Layouts
+import { HeaderOnly } from '~/components/Layout';
+
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload'
+import Search from '~/pages/Search';
 
 //Dùng cho Router không cần đăng nhập vẫn xem được
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
-    { path: '/profile', component: Profile }
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null }
 ]
 
 //Dùng cho Router buộc phải đăng nhập
