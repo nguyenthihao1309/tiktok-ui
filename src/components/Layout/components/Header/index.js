a41f0fd57e83ab5fe14ab6ad3693ce591cff81b5
nguyenthihao1309 <thihaonguyen1309@gmail.com>
2022-06-28 11:03:01 +0700

    handle click menu button

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,3 +1,4 @@
+
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless'
@@ -18,6 +19,21 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt'
+                }
+            ]
+        }
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -38,6 +54,16 @@ function Header() {
             setSearchResult([1, 2])
         }, 0)
     }, []);
+
+    //handle logic
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                //Handle Change Language
+                break;
+            default:
+        }
+    }
 
     return (
         <header className={cx('wrapper')}>
@@ -75,6 +101,7 @@ function Header() {
 
                     <Menu
                         items={MENU_ITEMS}
+                        onChange={handleMenuChange}
                     >
                         <button className={cx('more-button')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -7,8 +7,17 @@ import Button from "~/components/Button";
 
 const cx = classNames.bind(styles)
 
-function MenuItem({ data }) {
-    return (<Button className={cx('menu-item')} leftIcon={data.icon} to={data.to}>{data.title}</Button>);
+function MenuItem({ data, onClick }) {
+    return (
+        <Button
+            className={cx('menu-item')}
+            leftIcon={data.icon}
+            to={data.to}
+            onClick={onClick}
+        >
+            {data.title}
+        </Button>
+    );
 }
 
 export default MenuItem;
